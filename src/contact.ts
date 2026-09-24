c513f6ded17a1e337f6cc91e72ee0b2c9956d833
scjorge1908-cyber <scjorge1908@gmail.com>
2026-09-24 19:47:46 -0300

    Centralize Clínica Hope WhatsApp contact information

---
+++ src/contact.ts
@@ -0,0 +1,11 @@
+/**
+ * Centralized clinic contact information.
+ *
+ * To change the WhatsApp number in the future, edit only WHATSAPP_NUMBER below.
+ * Store it in international format, digits only: country code + area code + number.
+ */
+export const WHATSAPP_NUMBER = '5548998101804';
+
+export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
+
+export const WHATSAPP_DISPLAY_NUMBER = '(48) 9 9810-1804';
