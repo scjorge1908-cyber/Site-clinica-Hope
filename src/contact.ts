ed07b3e5b3072c353121d74fd2afa16a93e9608a
scjorge1908-cyber <scjorge1908@gmail.com>
2026-09-25 07:11:53 -0300

    Update contact.ts

--- src/contact.ts
+++ src/contact.ts
@@ -4,8 +4,8 @@
  * To change the WhatsApp number in the future, edit only WHATSAPP_NUMBER below.
  * Store it in international format, digits only: country code + area code + number.
  */
-export const WHATSAPP_NUMBER = '5548998101804';
+export const WHATSAPP_NUMBER = '554898385204';
 
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}`;
 
-export const WHATSAPP_DISPLAY_NUMBER = '(48) 9 9810-1804';
+export const WHATSAPP_DISPLAY_NUMBER = '(48) 9 9838-5204';
